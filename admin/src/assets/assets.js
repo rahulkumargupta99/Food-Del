41229b836c7673469d84bcc77f9cc1ad3961aeb5
Rahul Kumar Gupta <105960990+rahulkumargupta99@users.noreply.github.com>
2024-10-22 16:26:06 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -4,7 +4,8 @@ import order_icon from './order_icon.png'
 import profile_image from './profile_image.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
-
+export const url = 'https://food-del-backend-yyf7.onrender.com'
+export const currency = '₹'
 export const assets ={
     logo,
     add_icon,
@@ -14,4 +15,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://food-del-backend-yyf7.onrender.com'
+
